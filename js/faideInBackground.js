ce724b7f88edbb82e0cdb378d9e4f56a3d511278
Andriy Fedorchenko <doksel7@gmail.com>
2018-04-03 22:42:50 +0300

    small change

--- js/faideInBackground.js
+++ js/faideInBackground.js
@@ -1,19 +1,10 @@
 $(document).ready(function(){
-    // function changeBackground(){
-    //     let i=1;
-    //     setInterval(()=>{
-    //         $("header").css("background", "url('./images/bg"+i+".jpg')");            
-    //             if(i==3) i=1;
-    //             else i++;
-    //     },5000)};
-    // changeBackground();
     function changeBackground(){
-        let i=3;
+        let i=1;
         setInterval(()=>{
-            // $("header:nth-child(i)").css("background", "url('./images/bg"+i+".jpg')");   
-            $(`.{$i}`).fadeOut("slow");         
-                if(i==1) i=3;
-                else i--;
-        },2000)};
+            $("header").css("background", "url('./images/bg"+i+".jpg')");            
+                if(i==3) i=1;
+                else i++;
+        },5000)};
     changeBackground();
 });
