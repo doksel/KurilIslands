786af9bd8d02b26c2cbf49f3459d6477fdeb18b7
Andriy Fedorchenko <doksel7@gmail.com>
2018-04-05 11:52:55 +0300

    added changeBackground

--- js/faideInBackground.js
+++ js/faideInBackground.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
     function changeBackground(){
         let i=1;
         setInterval(()=>{
-            $("header").css("background", "url('./images/bg"+i+".jpg')", "background-size", "cover");            
+            $("header").css({
+                "background": "url('./images/bg"+i+".jpg') no-repeat center", 
+                "background-size": "cover","opacity":"0.4"})
+                .animate({opacity:'1'},2000);
                 if(i==3) i=1;
-                else i++;
-        },5000)};
+                else i++;    
+        },3000)};
+
     changeBackground();
 });
