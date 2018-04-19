8f159b2fd75b94bab5dc236d6f5ad0231b2c0986
Andriy Fedorchenko <doksel7@gmail.com>
2018-04-19 10:18:10 +0300

    added showForm

--- js/showForm.js
+++ js/showForm.js
@@ -1,1 +1,9 @@
-const form = document.querySelector(".form");
+$(document).ready(function(){
+    const form = document.querySelector(".form");
+    const contact = document.querySelector(".contact");
+
+    $(".contact").click(function(){
+        $(".form").css("opacity", "0")
+        .animate({opacity:'1'},2000);
+    });
+});
